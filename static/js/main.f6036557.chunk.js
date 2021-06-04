f04a7084caf6ff001584442b4d5b3c3a66be5de6
Code Sandbox <hello@codesandbox.io>
2021-06-04 09:18:36 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.f6036557.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpintroduction-to-jsx"]=this["webpackJsonpintroduction-to-jsx"]||[]).push([[0],{3:function(t,o,e){"use strict";e.r(o);var n=e(2);e(1);e(6).render(Object(n.jsx)("h1",{children:"Hello World"}),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.f6036557.chunk.js.map
